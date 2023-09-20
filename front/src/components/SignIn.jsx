84be533c3323dc126f8e55a7886d69be13e21395
AurelienAllenic <aurelien.allenic@gmail.com>
2023-09-20 15:15:52 +0200

    (feat) added redux store for login and slices too, it works, now i have to connect the back

--- front/src/components/SignIn.jsx
+++ front/src/components/SignIn.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from './Nav'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import { useDispatch } from 'react-redux';
+import { login } from '../features/Slices';
 const SignIn = () => {
+
+  const [name, setName] = useState('');
+  const [password, SetPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const dispatch = useDispatch();
+ const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    console.log(rememberMe, name, password)
+    e.preventDefault();
+    dispatch(login({
+        name: name,
+        rememberMe : rememberMe,
+        password: password,
+        loggedIn: true,
+    }))
+    navigate('/user')
+}
+
   return (
     <div className='signIn'>
       <Nav />
@@ -10,23 +31,22 @@ const SignIn = () => {
           <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign In</h1>
-            <form>
+            <form onSubmit={(e) => {
+        handleSubmit(e)
+      }}>
               <div className="input-wrapper">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" value={name} onChange={(e) => setName(e.target.value)} required/>
               </div>
               <div className="input-wrapper">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" value={password} onChange={(e) => SetPassword(e.target.value)} required/>
               </div>
               <div className="input-remember">
-                <input type="checkbox" id="remember-me" />
+                <input type="checkbox" id="remember-me" value={rememberMe} onChange={setRememberMe(!rememberMe)}/>
                 <label htmlFor="remember-me">Remember me</label>
               </div>
-              {/* PLACEHOLDER DUE TO STATIC SITE */}
-              <Link to="/user" className="sign-in-button">Sign In</Link>
-              {/*SHOULD BE THE BUTTON BELOW */}
-              {/* <button class="sign-in-button">Sign In</button> */}
+              <button type='submit' className="sign-in-button">Sign In</button>
             </form>
           </section>
         </main>
