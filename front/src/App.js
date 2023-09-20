9e63408d662efa69b3f615f7108839464b00e596
AurelienAllenic <aurelien.allenic@gmail.com>
2023-09-20 14:44:20 +0200

    (feat) added footer, signin and user pages with the html provided for the project, now, redux to work with

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import User from "./components/User";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/sign-in" element={<SignIn />} />
+          <Route exact path="/user" element={<User />} />
         </Routes>
       </div>
     </Router>
--- front/src/components/SignIn.jsx
+++ front/src/components/SignIn.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Nav from './Nav'
+import { Link } from 'react-router-dom'
+import Footer from './Footer'
 const SignIn = () => {
   return (
     <div className='signIn'>
-      SignIn
+      <Nav />
+        <main className="main bg-dark">
+          <section className="sign-in-content">
+            <i className="fa fa-user-circle sign-in-icon"></i>
+            <h1>Sign In</h1>
+            <form>
+              <div className="input-wrapper">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+              </div>
+              <div className="input-remember">
+                <input type="checkbox" id="remember-me" />
+                <label htmlFor="remember-me">Remember me</label>
+              </div>
+              {/* PLACEHOLDER DUE TO STATIC SITE */}
+              <Link to="/user" className="sign-in-button">Sign In</Link>
+              {/*SHOULD BE THE BUTTON BELOW */}
+              {/* <button class="sign-in-button">Sign In</button> */}
+            </form>
+          </section>
+        </main>
+    <Footer />
     </div>
   )
 }
