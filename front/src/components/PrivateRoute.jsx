96da3b7b7064a09516f7c82b187ff5ce11730a92
AurelienAllenic <aurelien.allenic@gmail.com>
2023-10-16 13:15:10 +0200

    (fix) changed wrong route from /sign-in to /login

--- front/src/components/PrivateRoute.jsx
+++ front/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function PrivateRoute(props) {
-  return localStorage.token ? props.children : <Navigate to="/sign-in" />;
+  return localStorage.token ? props.children : <Navigate to="/login" />;
 }
 
 export default PrivateRoute;
